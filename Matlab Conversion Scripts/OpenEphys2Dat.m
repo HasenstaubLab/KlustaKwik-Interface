d03b8ab346c4049462f28eb7b574a428b9f54fa7
astrasb <astra.bryant@ucsf.edu>
2015-03-11 16:03:26 -0700

    UI added to OpenEphys2Dat.m
    Added functionality: user interface queries which channels to save in
    .dat file.

--- Matlab Conversion Scripts/OpenEphys2Dat.m
+++ Matlab Conversion Scripts/OpenEphys2Dat.m
@@ -4,18 +4,22 @@ function OpenEphys2Dat
 %The desired .dat file format is [Time1Channel1, Time1Channel2...
 %Time1ChannelN...TimeNChannelN. 
 
-%So for a 16 channel probe, this program needs to a) read in the
+%So for an n channel probe, this program needs to a) read in the
 %.continous files, b) rearrange the raw waveform so that time moves iteratively
 %through each channel. 
 
-% Adjust parameter nchannelstosave in order to change how many channels will be saved.
+%The program allows the user to select which recorded channels are saved in
+%the .dat file. This is important because as of 3/11/15, Klustasuite in the
+%H-lab freezes on .dat files containing the full 16 channels of recording.
+%Unclear why. 
 
-% Astra Bryant 2/20/15
+% Astra Bryant 3/11/15
 
-nchannelstosave=3; %currently saves only the first n channels. Could write more complex code that asks exactly which channels should be saved. 
 
 dirname = uigetdir('Z:\astra\OpenEphys sample data\', 'Select Data Directory');
 cd(dirname);
+
+     
 
 temp=regexp(dirname,'\','split');
 filename= strcat(temp{end},'.dat');
@@ -27,14 +31,23 @@ fileList = {dirData(~dirIndex).name}';
 sortedfile=[1;9;10;11;12;13;14;15;16;2;3;4;5;6;7;8]; %reordering channels. works.
 fileList=fileList(sortedfile);
 
+temp=regexp(fileList,'CH\d*','match');
+for i=1:length(temp)
+    chanList(i)=temp{i};
+end
+chanList=chanList';
+
 dirADC=dir(fullfile(dirname,'100_ADC*.continuous'));
 ADCIndex=[dirADC.isdir];
 ADCList={dirADC(~ADCIndex).name}';
 
+%Ask user which channels to save
+[selection, ok] = listdlg('PromptString', 'Select channels to save in .dat file', 'SelectionMode','multiple', 'ListString',chanList);
+nchannelstosave=numel(selection);
 
 %for i=1:size(fileList,1)
 for i=1:nchannelstosave	
-	[data]=load_open_ephys_data(fileList{i});
+	[data]=load_open_ephys_data(fileList{selection(i)});
 	compile(i,:)=double(data);
 	clear data
 end
